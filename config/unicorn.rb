7c80bcaf4681e14f322808773c058cf16bc5f5a8
Sam <sameisenstat@yahoo.com>
2012-08-27 15:17:16 -0400

    Corrected paths in config files

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -28,7 +28,7 @@ before_fork do |server, worker|
   defined?(ActiveRecord::Base) and
     ActiveRecord::Base.connection.disconnect!
 
-  old_pid = RAILS_ROOT + '/tmp/pids/unicorn.pid.oldbin'
+  old_pid = app_root + '/tmp/pids/unicorn.pid.oldbin'
   if File.exists?(old_pid) && server.pid != old_pid
     begin
       Process.kill("QUIT", File.read(old_pid).to_i)
