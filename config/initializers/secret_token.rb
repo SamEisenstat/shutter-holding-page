3d25a03b79410294b1bec3e7cf0edbd4f8928d67
Sam <sameisenstat@yahoo.com>
2012-12-29 01:55:42 -0500

    Added secret_token.rb to .gitignore.

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-ShutterkickHoldingPage::Application.config.secret_token = 'ec75aa5d2d56d411ba6b0b61030e590040225ca92d04a68dc0f668f22ddfad7500e17007dc444ac50805b8d77c3aa96584e67b476c5577fb886d5df6b4c4cc25'
